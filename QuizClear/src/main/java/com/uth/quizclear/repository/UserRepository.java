eadc6c9531d1f002cdfe426f8d83a97eb4c2cf03
nkhoa <nkhoa3625@gmail.com>
2025-06-17 21:46:35 +0700

    fix profile

--- QuizClear/src/main/java/com/uth/quizclear/repository/UserRepository.java
+++ QuizClear/src/main/java/com/uth/quizclear/repository/UserRepository.java
@@ -99,12 +99,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<com.uth.quizclear.model.dto.UserBasicDTO> findUserBasicDTOById(@Param("userId") Long userId);
     
     @Query("SELECT new com.uth.quizclear.model.dto.UserBasicDTO(u.userId, u.fullName, u.email, CAST(u.role AS string), u.department, u.avatarUrl) FROM User u WHERE u.userId IN :userIds")
-    List<com.uth.quizclear.model.dto.UserBasicDTO> findUserBasicDTOsByIds(@Param("userIds") List<Long> userIds);    // Find user basic info by userId (for Profile)  
+    List<com.uth.quizclear.model.dto.UserBasicDTO> findUserBasicDTOsByIds(@Param("userIds") List<Long> userIds);
     @Query("SELECT new com.uth.quizclear.model.dto.UserBasicDTO("
             + "u.userId, u.fullName, u.email, u.role, u.department, "
             + "u.gender, u.dateOfBirth, u.nation, u.phoneNumber, "
             + "u.hometown, u.contactAddress, u.start, u.end, u.status, "
             + "u.workPlace, u.qualification) "
             + "FROM User u WHERE u.userId = :userId")
-    Optional<com.uth.quizclear.model.dto.UserBasicDTO> findUserBasicDTOByUserId(@Param("userId") Long userId);
+    Optional<com.uth.quizclear.model.dto.UserBasicDTO> findUserBasicDTOByUserId(@Param("userId") Long userId); // Find user basic info by userId (for Profile)  
 }
