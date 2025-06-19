b86d8e6abdf517e798cc0c3847cfefa067f39f76
HaojackYT <tangchihao4954@gmail.com>
2025-06-19 14:34:18 +0700

    finish Pending Approval in ExamManagement

--- QuizClear/src/main/java/com/uth/quizclear/controller/ExamManagementController.java
+++ QuizClear/src/main/java/com/uth/quizclear/controller/ExamManagementController.java
@@ -5,13 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/staff/exams")
 public class ExamManagementController {
     @Autowired
     private ExamService examService;
 
-    @GetMapping("/staff/exams")
+    @GetMapping("/all-exams")
     public String allExams(Model model) {
         model.addAttribute("exams", examService.getAllExams());
         model.addAttribute("totalExams", examService.countAll());
@@ -19,5 +21,15 @@ public class ExamManagementController {
         model.addAttribute("approvedCount", examService.countByStatus("approved"));
         model.addAttribute("rejectedCount", examService.countByStatus("rejected"));
         return "Staff/staffExamManagement_All_Exams";
+    }
+
+    @GetMapping("/pending-approval")
+    public String manageExams(Model model) {
+        model.addAttribute("totalExams", examService.countAll());
+        model.addAttribute("pendingCount", examService.countByStatus("submitted"));
+        model.addAttribute("approvedCount", examService.countByStatus("approved"));
+        model.addAttribute("rejectedCount", examService.countByStatus("rejected"));
+        model.addAttribute("pendingExams", examService.getPendingApprovalExams());
+        return "Staff/Staff_Exammanage";
     }
 }
