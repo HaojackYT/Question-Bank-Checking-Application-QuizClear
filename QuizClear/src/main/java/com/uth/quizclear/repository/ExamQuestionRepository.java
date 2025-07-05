e9155c41849409aef2111fa362f42fea05801114
HaojackYT <tangchihao4954@gmail.com>
2025-07-05 11:25:06 +0700

    Thymeleaf Export Exam

--- QuizClear/src/main/java/com/uth/quizclear/repository/ExamQuestionRepository.java
+++ QuizClear/src/main/java/com/uth/quizclear/repository/ExamQuestionRepository.java
@@ -11,4 +11,6 @@ import com.uth.quizclear.model.entity.ExamQuestion;
 public interface ExamQuestionRepository extends JpaRepository<ExamQuestion, Long> {
     List<ExamQuestion> findByExam_ExamIdOrderByQuestionOrder(Long examId);
     List<ExamQuestion> findByExamExamId(Long examId);
+
+    List<ExamQuestion> findByExamOrderByQuestionOrderAsc(com.uth.quizclear.model.entity.Exam exam);
 }
