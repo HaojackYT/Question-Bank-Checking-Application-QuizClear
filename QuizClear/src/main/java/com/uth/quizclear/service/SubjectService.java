20b876237a8e5a7b0221170c5c965d8cf52f4f9e
HaojackYT <tangchihao4954@gmail.com>
2025-07-06 13:13:19 +0700

    in progress create New Exam

---
+++ QuizClear/src/main/java/com/uth/quizclear/service/SubjectService.java
@@ -0,0 +1,20 @@
+package com.uth.quizclear.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.uth.quizclear.model.entity.Subject;
+import com.uth.quizclear.repository.SubjectRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SubjectService {
+    private final SubjectRepository subjectRepository;
+
+    public List<Subject> getAll() {
+        return subjectRepository.findAll();
+    }
+}
