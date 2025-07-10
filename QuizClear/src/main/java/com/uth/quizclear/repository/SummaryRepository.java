6e079230adf9aefa5f4400db8ee9bb2bae7d70ec
nkhoa <nkhoa3625@gmail.com>
2025-07-10 15:09:46 +0700

    fix

--- QuizClear/src/main/java/com/uth/quizclear/repository/SummaryRepository.java
+++ QuizClear/src/main/java/com/uth/quizclear/repository/SummaryRepository.java
@@ -35,22 +35,4 @@ public interface SummaryRepository extends JpaRepository<SummaryReport, Long> {
     @Query("SELECT u FROM User u WHERE u.role IN (:roles) AND u.status = 'ACTIVE' AND u.isLocked = false")
     List<User> findRecipient(@Param("roles") List<UserRole> roles);
 
-   // Truy vấn để lấy danh sách câu hỏi đã được phê duyệt
-    // Query to get list of approved questions
-    // Lưu ý: Sử dụng CASE WHEN để chuyển đổi enum DifficultyLevel thành String
-    // Note: Using CASE WHEN to convert DifficultyLevel enum to String
-    // @Query("SELECT new com.uth.quizclear.model.dto.QuesReportDTO(" +
-    //    "q.questionId, q.createdBy.fullName, " +
-    //    "CASE q.difficultyLevel " +
-    //    "    WHEN com.uth.quizclear.model.enums.DifficultyLevel.RECOGNITION THEN 'recognition' " +
-    //    "    WHEN com.uth.quizclear.model.enums.DifficultyLevel.COMPREHENSION THEN 'comprehension' " +
-    //    "    WHEN com.uth.quizclear.model.enums.DifficultyLevel.BASIC_APPLICATION THEN 'Basic Application' " +
-    //    "    WHEN com.uth.quizclear.model.enums.DifficultyLevel.ADVANCED_APPLICATION THEN 'Advanced Application' " +
-    //    "    ELSE 'Unknown' " +
-    //    "END, " +
-    //    "q.createdAt, CONCAT('', q.status)) " +
-    //    "FROM Question q " +
-    //    "WHERE q.status = com.uth.quizclear.model.enums.QuestionStatus.APPROVED")
-    // List<QuesReportDTO> findApprovedQuestions();
-
 }
